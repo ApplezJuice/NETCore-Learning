6e75d1cb6bc30fb413178a424c177e72d1c0ab16
Michael Hayes <mhayes@blizzard.com>
2019-07-31 11:23:55 -0700

    First commit

---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            // dependency injection using iconfiguration service
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                //DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
+                //{
+                //    SourceCodeLineCount = 10
+                //};
+                app.UseDeveloperExceptionPage();
+            }
+
+            // file server over UseStaticFiles and UseDefaultFiles allows us to combine the functionality of the 2 and
+            // add in UseDirectoryBrowser middleware
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+
+            //app.UseFileServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
+            {
+                //throw new Exception("Some error processing the request");
+                await context.Response.WriteAsync("Hosting Environment: " + env.EnvironmentName);
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# NETCore-Learning" 
