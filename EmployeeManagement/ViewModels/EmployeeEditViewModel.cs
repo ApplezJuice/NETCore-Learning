4356362b744cf5d208961ad10419bba25631751c
Michael Hayes <mhayes@blizzard.com>
2019-08-13 13:20:58 -0700

    Added in edit view

---
+++ EmployeeManagement/ViewModels/EmployeeEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeEditViewModel : EmployeeCreateViewModel
+    {
+        // Inheriting already provides the most of the properties
+        public int Id { get; set; }
+        public string ExistingPhotoPath { get; set; }
+    }
+}
